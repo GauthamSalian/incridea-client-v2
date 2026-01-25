de6f0a07ea6e2befe5f57bc598e7f01574e1776c
GauthamSalian <gauthamsalian06@gmail.com>
2026-01-25 11:44:26 +0530

    Add Coming Soon page and update routing

---
+++ src/pages/ComingSoon.tsx
@@ -0,0 +1,10 @@
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon">
+      <h1>Coming Soon</h1>
+      <p>We are working hard to launch our new website. Stay tuned!</p>
+    </div>
+  );
+};
+
+export default ComingSoon;
